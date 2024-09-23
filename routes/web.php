c544764b2742d6ffb4973dd8a64c0c738b7ee6d6
yunamatsuzaki <yunamatsuzaki@icloud.com>
2024-09-23 22:02:46 +0900

    Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
@@ -17,9 +19,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth');
+
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
 
 Route::get('/posts/{id}', [PostController::class, 'show']);
+
+require __DIR__.'/auth.php';
---
+++ routes/web_backup.php
@@ -0,0 +1,25 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+
+Route::get('/posts/{id}', [PostController::class, 'show']);
